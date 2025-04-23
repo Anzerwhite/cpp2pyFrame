600c46b729a801ed25be19019470359b12b99375
Anzerwhite <131354484+Anzerwhite@users.noreply.github.com>
2025-04-23 17:20:30 +0800

    Update cppCaller.h

--- cppFiles/cppCaller.h
+++ cppFiles/cppCaller.h
@@ -8,9 +8,9 @@ public:
 	CppCaller();
 	~CppCaller();
 
+	// 你自己的业务函数
 	std::vector<int> testFn(std::vector<int> data);
-
 private:
+	// 该类对应调用的python模块，可写多个，但从职责设计角度上建议只调用一个模块，减少耦合
 	PyObject* pModule;
-
 };
